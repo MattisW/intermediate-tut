f06c3f781bc4d17bda7e15670ef32a2bc20e5129
MattisW <mattis@nameless-crew.de>
2015-11-24 15:53:44 +0100

    #13: Template level subscriptions
    +added: spinner package
    +learned: instead of subscribing the usual way, i can use template level
    subscriptions which will only fire whenever the template is used
    +added: Template level subscription
    +added: #if Template.subscriptionsReady block with a loading fallback

--- client/layouts/recipes/Recipes.js
+++ client/layouts/recipes/Recipes.js
@@ -1,4 +1,16 @@
-Meteor.subscribe("recipes");
+// Meteor.subscribe("recipes");
+
+// INSTEAD OF USING THE REGULAR GENERIC SUBSCRIPTION
+// EVERYWHERE WE SUBSCRIBE ONLY ON THE TEMPLATE LEVEL
+// ON ITS CREATION. ONCE CREATED, WE SUBSCRIBE IN AN
+// AUTORUN TO CALL THE SUBSCRIBTION EACH TIME THE
+// TEMPLATE IS USED
+Template.Recipes.onCreated(function() {
+    var self = this;
+    self.autorun(function() {
+        self.subscribe('recipes');
+    });
+});
 
 Template.Recipes.helpers({
     recipes: ()=> {
